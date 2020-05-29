9307edee4e06dc5d01d2e4df1fa39bd4cecff3ba
mihir1337 <mihir.mahamud@gmail.com>
2020-05-29 22:12:36 +0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+(function ($) {
+    'use strict';
+
+    $(window).on("load", function() {
+		background();
+	    //preloader
+		$("#preloader").delay(300).animate({
+	    	"opacity" : "0"
+	    	}, 500, function() {
+	    	$("#preloader").css("display","none");
+	    });
+	});
+
+    // background image js
+	function background() {
+			var img=$('.bg_img');
+			img.css('background-image', function () {
+			var bg = ('url(' + $(this).data('background') + ')');
+			return bg;
+		});
+	}
+
+	// Nice select
+	$('select').niceSelect();
+
+	// offcanvas menu
+	$(".menu-tigger").on("click", function () {
+		$(".extra-info,.offcanvas-overly").addClass("active");
+		return false;
+	});
+	$(".menu-close,.offcanvas-overly").on("click", function () {
+		$(".extra-info,.offcanvas-overly").removeClass("active");
+	});
+
+
+	// Activate lightcase
+    $('a[data-rel^=lightcase]').lightcase();
+
+})(jQuery);
